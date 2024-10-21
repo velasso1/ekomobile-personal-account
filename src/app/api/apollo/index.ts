c68abdf8791ba1a64397ef1478507405c4948b63
Bogdan Selenginskii <b.selenginskii@ekomobile.ru>
2024-10-21 16:51:58 +0300

    resolve-conflicts
    - attempt rewrite localStorage logic

--- src/app/api/apollo/index.ts
+++ src/app/api/apollo/index.ts
@@ -4,7 +4,7 @@ const client = new ApolloClient({
   uri: "https://api-lk2.ekomobile.ru/graphql",
   headers: {
     "X-Auth-Client-Key": `${import.meta.env.VITE_TEMP_TOKEN}`,
-    "X-Auth-Token": `${localStorage.getItem("token")}`,
+    // "X-Auth-Token": `${localStorage.getItem("token")}`,
   },
   cache: new InMemoryCache(),
   credentials: "include",
